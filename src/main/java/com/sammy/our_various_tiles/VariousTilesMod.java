9cd991333060f9bb11738977fdc9d6a29eaa3feb
Sammy <malummoddevelopment@gmail.com>
2021-07-08 01:40:56 +0200

    Initial commit

---
+++ src/main/java/com/sammy/our_various_tiles/VariousTilesMod.java
@@ -0,0 +1,15 @@
+package com.sammy.our_various_tiles;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("our_various_tiles")
+public class VariousTilesMod
+{
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public VariousTilesMod()
+    {
+    }
+}
